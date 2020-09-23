dac9fa12afd7e02ac568f304232cfe689fe5b6ce
Victor Guedes <victorfelipe.guedes@hotmail.com>
2020-09-22 22:10:46 -0300

    :sparkles: get random quote && css formated

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,33 @@
-// alert("Initial commit");
+const randomButton = document.querySelector("#random");
+
+randomButton.addEventListener("click", async () => getRandom());
+
+async function getRandom() {
+  const url = "https://quote-garden.herokuapp.com/api/v2/quotes/random";
+  const quoteContent = document.querySelector("#quote");
+  const quoteAuthor = document.querySelector("#name");
+  const quoteGenre = document.querySelector("#genre");
+
+  await fetch(url)
+    .then(function (response) {
+      if (!response.ok)
+        throw new Error(
+          "Service unavailable, try again later. Status: " + response.status
+        );
+
+      return response.json();
+    })
+    .then(function (data) {
+      quoteContent.innerHTML = "“" + data.quote.quoteText + "”";
+      quoteAuthor.innerHTML = data.quote.quoteAuthor
+        ? data.quote.quoteAuthor
+        : "unspecified";
+      quoteGenre.innerHTML =
+        data.quote.quoteGenre != undefined
+          ? data.quote.quoteGenre
+          : "unspecified";
+    })
+    .catch(function (error) {
+      console.error(error.message);
+    });
+}
